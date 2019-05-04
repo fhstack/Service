41a337c6f1bf50d43d53a59389386fcfa7374b14
l-f-h <1606623972@qq.com>
2019-05-04 17:23:08 +0800

    vendor

---
+++ vendor/github.com/l-f-h/judge/conf/conf.go
@@ -0,0 +1,29 @@
+package conf
+
+const (
+	RpcPort string = ":10000"
+)
+
+const (
+	RedisIp       string = "39.107.83.63"
+	RedisPort     string = "6379"
+	RedisPassword string = ""
+)
+
+const (
+	MysqlIp       string = "127.0.0.1"
+	MysqlPort     string = "3306"
+	MysqlUser     string = "root"
+	MysqlPassword string = "liufeihao534"
+	MysqlDb       string = "oj"
+)
+
+const (
+	SourceCodeName       string = "SourceCode"
+	UserSubmitHome       string = "/home/judge/submit/"
+	ProblemDataHome      string = "/home/judge/problem/"
+	SubmitPrefix         string = "submit_"
+	GeneratedProgramName string = "main"
+	RunPath              string = "/home/fh/goWorkspace/src/github.com/l-f-h/judge/inline/Run"
+	ComparePath          string = "/home/fh/goWorkspace/src/github.com/l-f-h/judge/inline/Compare"
+)
